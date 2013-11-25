9ade4cfcccb43a59bd05f28c84155dbba5cc043e
dhendiii <dhendiawandwiatma@gmail.com>
2013-11-26 06:49:16 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+
+<body>
+<div id=Content>
+<h1>Tugas Pemograman Integratif</h1>
+<h3>oleh :</h3>
+<ul style="font-size:15px; list-style-type: none;text-align:center;">
+    <li>Dhendiawan Dwiatma (18210002) </li>
+    <li>Rizki Novian (18210016)</li>
+</ul>
+<h2> Link URI </h2>
+
+<form action="protif.php" method="get" style="text-align:center;">
+
+    <select name="uri">
+        <option value="" selected="selected">Pilih file xml</option>
+        <option value="http://localhost/progin-itb/BernadetteVina/myxmlfile.xml">BernadetteVina</option>
+        <option value="http://localhost/progin-itb/18211010-18211035/progin.xml">18211010-...</option>
+        <option value="http://localhost/progin-itb/BintangAdinandra/menu.xml">BintangAdinandra</option>
+        <option value="http://localhost/progin-itb/II3160--Pemrograman-Integratif-/DaftarIdol.xml">II3160--Pe…</option>
+        <option value="http://localhost/progin-itb/II3160-Tugas1-Tugas2/tab2.xml?submit=Membuat+XML+dari+XML">II3160-Tugas1...</option>
+        <option value="http://localhost/progin-itb/II3160-Tugas2-18211055-18211026/index.php/api/test/juses">II3160-Tugas2...</option>
+        <option value="http://localhost/progin-itb/IPT-Assignments/data1.xml">IPT-Assignment</option>
+        <option value="http://localhost/progin-itb/Pemrograman-Intergratif/dbxml.xml">Pemograman-...</option>
+        <option value="http://localhost/progin-itb/Workspace/Menu.xml">Workspace</option>
+        <option value="http://localhost/progin-itb/pemrograman_integratif/output.xml">pemrograman_...</option>
+        <option value="http://localhost/progin-itb/progin/contoh.xml">progin</option>
+    </select>
+<br>
+<br>
+<input type="submit" value ="Go!">
+</form>
+
+</div>
+</body>
+</html>
+
